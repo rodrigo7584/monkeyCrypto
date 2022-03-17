edb9ef729be59578b6f4ef449051f250a5663302
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2022-03-16 21:51:29 -0300

    adicao gliderjs

--- js/script.js
+++ js/script.js
@@ -3,3 +3,30 @@ const iconeMobile = document.querySelector('.icone-menu-mobile')
 btnMenuMobile.addEventListener('click', function () {
   iconeMobile.classList.toggle('ativo')
 })
+
+const caroussel = document.querySelector('.caroucel');
+//É mobile first então vai mostrar 1 slide com a menor resolução
+new Glider(caroussel,{
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  draggable: true,
+  arrows:{
+    prev: ".avanca",
+    next: ".volta"
+  },
+  scrollLock:true,
+  responsive:[{
+    breakpoint: 600,
+    settings:{
+      slidesToShow: 2,
+      slidesToScroll:2
+    },
+    breakpoint: 900,
+    settings:{
+      slidesToShow: 3,
+      slidesToScroll:3
+    },
+  }]
+})
+
+// https://desenvolvimentoparaweb.com/javascript/gliderjs-slider-carousel/
